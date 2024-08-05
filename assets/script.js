ad518db28872d1556bf7d711e51931b33e42bd4e
Kelve Oliveira <kelveoliveira052@gmail.com>
2024-08-05 19:48:37 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -34,9 +34,13 @@ const adiconarTarefa = (tarefa, data) => {
     const tdTarefa = document.createElement('td')
     const tdData = document.createElement('td')
     const trash = document.createElement('img')
+    const edit = document.createElement('img')
 
     tdTarefa.innerText = tarefa
     tdData.innerText = data
+    tdTarefa.id = `tarefa${contador}`
+    tdData.id = `data${contador}`
+
     trash.src = 'imgs/trash.svg'
     trash.id = contador
     trash.className = 'trash'
@@ -44,6 +48,11 @@ const adiconarTarefa = (tarefa, data) => {
     trash.addEventListener('click', () => {
       excluir(trash.id)
     })
+
+    edit.src = 'imgs/edit.svg'
+    edit.className = 'trash edit'
+    edit.id = `edit${contador}`
+    edit.addEventListener('click', editar(tdTarefa.id, tdData.id))
     tdTarefa.className = 'dados-livro'
     tdData.className = 'dados-livro'
 
@@ -52,6 +61,7 @@ const adiconarTarefa = (tarefa, data) => {
     linhaDeCadaTarefa.appendChild(tdTarefa)
     linhaDeCadaTarefa.appendChild(tdData)
     tdData.appendChild(trash)
+    tdData.appendChild(edit)
 
     bodyTable.insertAdjacentElement("afterbegin", linhaDeCadaTarefa);
     exibirMensagem()
@@ -177,22 +187,31 @@ const editar = (idTarefa, idData) => {
     const btnSalvar = document.getElementById('btn-salvar')
     btnSalvar.style.display = 'block'
     btnCancelar.style.display = 'block'
-    btnSalvar.onclick = (e)=>{
+    btnSalvar.onclick = (e) => {
       e.preventDefault()
-      arrayTarefas[indexTarefa].tarefa = inputTarefa.value
-      arrayTarefas[indexData].data = inputData.value
+      if (inputTarefa.value == '' || inputData.value == '') {
+        if (!inputTarefa.value) {
+          pMensagemErroTarefa.style.color = '#ffa600'
+          inputTarefa.className = 'input input-tremer'
+        }
+        if (!inputData.value) {
+          pMensagemErroData.style.color = '#ffa600'
+          inputData.className = 'input input-tremer'
+        }
+      } else {
+        arrayTarefas[indexTarefa].tarefa = inputTarefa.value
+        arrayTarefas[indexData].data = inputData.value
 
-
-
-      setLivros()
-      renderizarTarefasDoLocalStorage()
-      document.getElementById(idData).className = 'dados-livro dados-piscar'
-      document.getElementById(idTarefa).className = 'dados-livro dados-piscar'
-      btnSalvar.style.display = 'none'
-      btnCancelar.style.display = 'none'
-      btnAdicionar.style.display = 'block'
-      inputTarefa.value = ''
-      inputData.value = ''
+        setLivros()
+        renderizarTarefasDoLocalStorage()
+        document.getElementById(idData).className = 'dados-livro dados-piscar'
+        document.getElementById(idTarefa).className = 'dados-livro dados-piscar'
+        btnSalvar.style.display = 'none'
+        btnCancelar.style.display = 'none'
+        btnAdicionar.style.display = 'block'
+        inputTarefa.value = ''
+        inputData.value = ''
+      }
     }
   }
 }
